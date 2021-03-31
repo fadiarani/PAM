9e3a807a95ac890ebdfedd8dd3bd3db624a5d515
Fadia Rani <fadiarani9@gmail.com>
2021-03-31 15:41:26 +0700

    Activity 3

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sampleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txtStatus;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,26 +42,47 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+                String email = "lams@gmail.com";
+                String pass = "123";
+
                 if(nama.equals("lams@gmail.com") && password.equals("123")) {
                     Toast t = Toast.makeText(getApplicationContext(),
                             "Login Sukses", Toast.LENGTH_LONG);
                     t.show();
-                }
-                else if(!nama.equals("lams@gmail.com") && password.equals("123")) {
+
+                    Bundle b = new Bundle();
+
+                    b.putString("a", nama.trim());
+                    b.putString("b", password.trim());
+
+                    Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                    i.putExtras(b);
+                    startActivity(i);
+                } else {
                     Toast t = Toast.makeText(getApplicationContext(),
-                            "Email Salah", Toast.LENGTH_LONG);
+                            "Login Gagal", Toast.LENGTH_LONG);
                     t.show();
                 }
-                else if(nama.equals("lams@gmail.com") && !password.equals("123")) {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Password Salah", Toast.LENGTH_LONG);
-                    t.show();
-                }
-                else {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Email dan Password Salah", Toast.LENGTH_LONG);
-                    t.show();
-                }
+//                }
+//                else if(!nama.equals("lams@gmail.com") && password.equals("123")) {
+//                    Toast t = Toast.makeText(getApplicationContext(),
+//                            "Email Salah", Toast.LENGTH_LONG);
+//                    t.show();
+//                }
+//                else if(nama.equals("lams@gmail.com") && !password.equals("123")) {
+//                    Toast t = Toast.makeText(getApplicationContext(),
+//                            "Password Salah", Toast.LENGTH_LONG);
+//                    t.show();
+//                }
+//                else {
+//                    Toast t = Toast.makeText(getApplicationContext(),
+//                            "Email dan Password Salah", Toast.LENGTH_LONG);
+//                    t.show();
+//                }
             }
         });
     }
